6925ea8d384e64043ae7e06faf3977d0179c50b0
Fons Hettema <fons.hettema@hva.nl>
2016-04-15 13:40:45 +0200

    Meteor pub prototype

--- server/publications.js
+++ server/publications.js
@@ -11,6 +11,7 @@ Meteor.publish("markers", function publishFunction() {
 Meteor.startup(function() {
     //Markers.remove({});
     //Markers.update({_id: "B8Fx5qdp3syTZYbwv"}, {$set: {occupancy: 250, totalSpots: 520}});
+    //Markers.update({_id: "uowv6NRi4C8rPj8hg"}, {$set: {userId: "cPR5LgqHfEGwEzyC8"}});
     //Markers.update({}, {$set: {occupancy: 250, totalSpots: 520}});
     //Markers.update({}, {$set: {occupancy: 250, totalSpots: 520}}, {multi: true});
 });
