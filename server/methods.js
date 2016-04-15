bd4fa357c7f97668d71d1ed864a19d548f9384ea
Fons Hettema <fons.hettema@hva.nl>
2016-04-15 13:40:15 +0200

    API call method

--- server/methods.js
+++ server/methods.js
@@ -4,3 +4,16 @@ Meteor.methods({
         return Meteor.http.call("GET", "http://open.datapunt.amsterdam.nl/ivv/parkeren/locaties.json");
     }
 });
+
+// Meteor.call("getApiData", function(error, results) {
+//     var parkeerlocaties = results.data.parkeerlocaties;
+//     parkeerlocaties.forEach(function(storage, index) {
+//         var title = storage.parkeerlocatie.title;
+//         var coords = JSON.parse(storage.parkeerlocatie.Locatie);
+//         var lng = coords.coordinates[0];
+//         var lat = coords.coordinates[1];
+//         var adres = storage.parkeerlocatie.adres;
+//         var postcode = storage.parkeerlocatie.postcode;
+//         Markers.insert({ title: title, lat: lat, lng: lng, adres: adres, postcode: postcode, userId: 1, markerIcon: "images/green.png" });
+//     });
+// });
